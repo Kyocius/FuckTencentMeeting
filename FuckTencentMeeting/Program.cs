acc2b2eb5f8a318e937e2478448dc304341320a8
Yoroion <1738435753@qq.com>
2022-03-26 13:23:34 +0800

    添加项目文件。

---
+++ FuckTencentMeeting/Program.cs
@@ -0,0 +1,38 @@
+﻿using FuckTencentMeeting;
+using System.Diagnostics;
+using WindowsInput;
+
+Console.WriteLine("会议号？ ");
+string code = Console.ReadLine(); //会议号
+Console.WriteLine("预定时间？(天数/小时/分钟)  ");
+string now = Console.ReadLine(); //预定时间
+
+var dateTime = DateTime.Now.ToString("dd/H/m");
+
+while (true)
+{
+    dateTime = DateTime.Now.ToString("dd/H/m");
+    Console.WriteLine(dateTime);
+
+    if (now == dateTime)
+    {
+        Start(code);
+        break;
+    }
+}
+
+void Start(string code)
+{
+    //这里改成自己的安装路径
+    Process.Start("E:\\Tencent Room\\WeMeet\\wemeetapp.exe");
+    Thread.Sleep(3000);
+
+    Win32Method.LeftMouseClick(820, 319);
+    Thread.Sleep(500);
+
+    new InputSimulator().Keyboard.TextEntry(code);
+    Thread.Sleep(1000);
+
+    Win32Method.LeftMouseClick(959, 813);
+    Console.WriteLine("入会成功了吧");
+}
---
+++ FuckTencentMeeting/Win32Method.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FuckTencentMeeting
+{
+    public static class Win32Method
+    {
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        static extern bool SetCursorPos(int x, int y);
+
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        static extern void mouse_event(int dwFlags, int dx, int dy, int dwData, int dwExtraInfo);
+
+        private const int MOUSEEVENTF_LEFTDOWN = 0x02;
+        private const int MOUSEEVENTF_LEFTUP = 0x04;
+
+        internal static void LeftMouseClick(int Xposition, int Yposition)
+        {
+            SetCursorPos(Xposition, Yposition);
+            mouse_event(MOUSEEVENTF_LEFTDOWN, Xposition, Yposition, 0, 0);
+            mouse_event(MOUSEEVENTF_LEFTUP, Xposition, Yposition, 0, 0);
+        }
+    }
+}
